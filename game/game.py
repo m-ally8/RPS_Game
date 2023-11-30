4e700369deeaf8d7f68eaec28a1c40f80e4d9ed5
m-ally8 <143876631+m-ally8@users.noreply.github.com>
2023-11-30 15:06:55 +0300

    Added score feature to each scenario

--- game/game.py
+++ game/game.py
@@ -10,33 +10,53 @@ words = ['rock','paper','scissor']
 
 rand_word = random.choice(words)
 
+score = 0
+
 for i in range(1,4):
     guessed_word = input("Enter Your Choice: ").lower()
     if guessed_word == 'rock':
         if rand_word == 'rock':
+            score = score
             print("a tie")
+            print(score)
         elif rand_word == 'paper':
+            score = score - 1
             print("Paper wraps rock, YOU LOST")
+            print(score)
         elif rand_word == 'scissor':
+            score = score + 1
             print("Rock destroys scissor, YOU WON")
+            print(score)
         else:
             print("Invalid input!")
     elif guessed_word == 'paper':
         if rand_word == 'paper':
+            score = score 
             print("a tie")
+            print(score)
         elif rand_word == 'rock':
+            score = score + 1
             print("Paper wraps rock, YOU WON")
+            print(score)
         elif rand_word == 'scissor':
+            score = score - 1
             print("Scissor cuts paper, YOU LOST")
+            print(score)
         else:
             print("Invalid input!") 
     elif guessed_word == 'scissor':
         if rand_word == 'scissor':
+            score = score
             print("a tie")
+            print(score)
         elif rand_word == 'paper':
+            score = score + 1
             print("Scissor cuts paper, YOU WON")
+            print(score)
         elif rand_word == 'rock':
+            score = score - 1
             print("Rock destroys scissor, YOU LOST")
+            print(score)
         else:
             print("Invalid input!")                           
     else:
