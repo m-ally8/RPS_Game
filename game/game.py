c8c98c3efc12a3a2ff80c02d31365e964a299c21
m-ally8 <143876631+m-ally8@users.noreply.github.com>
2023-12-03 05:31:17 +0300

    Adding individual total score and general total score

--- game/game.py
+++ game/game.py
@@ -11,55 +11,64 @@ words = ['rock','paper','scissor']
 rand_word = random.choice(words)
 
 score = 0
+total_score1 = 0
+total_score2 = 0
+total_score3 = 0
 total_score = 0
 
 for i in range(1,4):
     guessed_word = input("Enter Your Choice: ").lower()
     if guessed_word == 'rock':
         if rand_word == 'rock':
-            score = score
+            total_score1 = score
             print("a tie")
-            print(f"SCORE: {score}")
+            print(f"SCORE: {total_score1}")
         elif rand_word == 'paper':
-            score = score - 1
+            total_score2 = total_score1 - 1
             print("Paper wraps rock, YOU LOST")
-            print(f"SCORE: {score}")
+            print(f"SCORE: {total_score2}")
         elif rand_word == 'scissor':
-            score = score + 1
+            total_score3 = total_score2 + 1
             print("Rock destroys scissor, YOU WON")
-            print(f"SCORE: {score}")
+            print(f"SCORE: {total_score3}")
         else:
             print("Invalid input!")
     elif guessed_word == 'paper':
         if rand_word == 'paper':
-            score = score 
+            total_score1 = score
             print("a tie")
-            print(f"SCORE: {score}")
+            print(f"SCORE: {total_score1}")
         elif rand_word == 'rock':
-            score = score + 1
+            total_score2 = total_score1 - 1
             print("Paper wraps rock, YOU WON")
-            print(f"SCORE: {score}")
+            print(f"SCORE: {total_score2}")
         elif rand_word == 'scissor':
-            score = score - 1
+            total_score3 = total_score2 + 1
             print("Scissor cuts paper, YOU LOST")
-            print(f"SCORE: {score}")
+            print(f"SCORE: {total_score3}")
         else:
             print("Invalid input!") 
     elif guessed_word == 'scissor':
         if rand_word == 'scissor':
-            score = score
+            total_score1 = score
             print("a tie")
-            print(f"SCORE: {score}")
+            print(f"SCORE: {total_score1}")
         elif rand_word == 'paper':
-            score = score + 1
+            total_score2 = total_score + 1
             print("Scissor cuts paper, YOU WON")
-            print(f"SCORE: {score}")
+            print(f"SCORE: {total_score2}")
         elif rand_word == 'rock':
-            score = score - 1
+            total_score3 = total_score2 - 1
             print("Rock destroys scissor, YOU LOST")
-            print(f"SCORE: {score}")
+            print(f"SCORE: {total_score3}")
         else:
             print("Invalid input!")                           
     else:
         print("Invalid input!")
+
+total_score = total_score1 + total_score2 + total_score3        
+
+
+print(f"TOTAL SCORE: {total_score}")
+
 
